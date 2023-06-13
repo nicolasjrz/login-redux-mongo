12f05544f02d82da3e729883ca261caf757c545f
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-06-13 20:16:33 -0300

    validations with reacto-hook-form

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,31 +1,52 @@
 import { Button, Grid, Link, TextField } from "@mui/material";
 import { LayoutAuth } from "../layout/LayoutAuth";
+import { useForm } from "react-hook-form";
 
 export const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(errors);
   return (
     <LayoutAuth title="Login">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               fullWidth
-              id="email"
-              label="Email"
               variant="standard"
               type="email"
+              label="Email"
+              {...register("email", {
+                required: "El correo electrónico es requerido",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Ingrese un correo electrónico válido",
+                },
+              })}
+              aria-invalid={errors.email ? "true" : "false"}
             />
+            {errors.email && <p role="alert">{errors.email?.message}</p>}
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               fullWidth
-              id="password"
-              label="Password"
               variant="standard"
               type="password"
+              label="Password"
+              {...register("password", {
+                required: "El password es requerido",
+              })}
+              aria-invalid={errors.password ? "true" : "false"}
             />
+            {errors.password && <span>password required</span>}
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth type="submit">
               Login
             </Button>
           </Grid>
