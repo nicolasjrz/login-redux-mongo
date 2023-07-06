71c748019423d852ef787e30053a14b514f5ddb4
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-05 22:52:57 -0300

    aplicated status in appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { WebRoutes } from "../web/routes/WebRoutes";
+import { useSelector } from "react-redux";
 
 export const AppRouter = () => {
-  const authStatus = "not-authenticated";
+  const { status } = useSelector((state) => state.auth);
 
   return (
     <Routes>
-      {authStatus === "not-authenticated" ? (
+      {status === "checking" ? (
         <Route path="/auth/*" element={<AuthRoutes />} />
       ) : (
         <Route path="/*" element={<WebRoutes />} />
