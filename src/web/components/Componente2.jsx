b98f1fcc98780a5ac81aa76b6f923b41fe56ba69
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-11 19:02:28 -0300

    tomo el valor de turno y lo uso en el imput y aplico un useEffect para usar next

--- src/web/components/Componente2.jsx
+++ src/web/components/Componente2.jsx
@@ -3,24 +3,25 @@ import { TextField, Typography } from "@mui/material";
 import { StepperLayout } from "../layout/StepperLayout";
 import { useDispatch } from "react-redux";
 import { onTurno } from "../../store/auth/turnoSlice";
+import { useEffect } from "react";
 
 export const Componente2 = ({ turno, setNext }) => {
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    turno === "" ? setNext(true) : setNext(false);
+  }, [turno, setNext]);
+
   const handleTurnoChange = (event) => {
-    const turno = event.target.value;
-
-    // if (event.target.value === 0)
-    //   return console.log("por favor ingrese un turno");
-
+    const nuevoTurno = event.target.value;
+    dispatch(onTurno(nuevoTurno));
     setNext(false);
-    dispatch(onTurno(turno));
   };
 
   return (
     <StepperLayout>
       <Typography>Turno : {turno}</Typography>
-      <TextField type="number" onChange={handleTurnoChange} />
+      <TextField type="number" onChange={handleTurnoChange} value={turno} />
     </StepperLayout>
   );
 };
