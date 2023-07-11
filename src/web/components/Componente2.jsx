b707fa5aac2c794ce2524764e13f7d4a0a06a8fc
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-11 01:12:56 -0300

    aplicacion de redux en componete2 (turno)

--- src/web/components/Componente2.jsx
+++ src/web/components/Componente2.jsx
@@ -1,10 +1,20 @@
-import { Typography } from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 import { StepperLayout } from "../layout/StepperLayout";
+import { useDispatch } from "react-redux";
+import { onTurno } from "../../store/auth/turnoSlice";
 
-export const Componente2 = () => {
+export const Componente2 = ({ turno }) => {
+  const dispatch = useDispatch();
+
+  const handleTurnoChange = (event) => {
+    const fecha = event.target.value;
+    dispatch(onTurno(fecha));
+  };
+
   return (
     <StepperLayout>
-      <Typography>Componente2</Typography>
+      <Typography>Componente2 : {turno}</Typography>
+      <TextField type="number" onChange={handleTurnoChange} />
     </StepperLayout>
   );
 };
