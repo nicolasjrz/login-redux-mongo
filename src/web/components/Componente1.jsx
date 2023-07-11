bacf7934a0a77d0d167ff7e2b4847b0597fc5eb0
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-11 18:59:42 -0300

    tomo el valor de fecha y lo uso en el input, aplicacion de useEffect para poder usar next

--- src/web/components/Componente1.jsx
+++ src/web/components/Componente1.jsx
@@ -3,24 +3,26 @@ import { StepperLayout } from "../layout/StepperLayout";
 
 import { useDispatch } from "react-redux";
 import { onDate } from "../../store/auth/turnoSlice";
+import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 export const Componente1 = ({ fecha, setNext }) => {
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    fecha === "" ? setNext(true) : setNext(false);
+  }, [fecha, setNext]);
+
   const handleFechaChange = (event) => {
-    const fecha = event.target.value;
-
-    // if (fecha === "") return console.log("por favor ingrese una fecha");
-
+    const nuevaFecha = event.target.value;
+    dispatch(onDate(nuevaFecha));
     setNext(false);
-    dispatch(onDate(fecha));
   };
 
   return (
     <StepperLayout>
       <Typography>Fecha : {fecha}</Typography>
-      <TextField type="date" onChange={handleFechaChange} />
+      <TextField type="date" onChange={handleFechaChange} value={fecha} />
     </StepperLayout>
   );
 };
