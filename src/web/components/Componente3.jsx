02520b995fbc567a24e7b48bc561d68b46b9d68b
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-11 19:04:32 -0300

    tomo el valor de servicio  y lo uso en el imput y aplico un useEffect para usar next

--- src/web/components/Componente3.jsx
+++ src/web/components/Componente3.jsx
@@ -3,22 +3,28 @@ import { TextField, Typography } from "@mui/material";
 import { StepperLayout } from "../layout/StepperLayout";
 import { useDispatch } from "react-redux";
 import { onService } from "../../store/auth/turnoSlice";
+import { useEffect } from "react";
 
 export const Componente3 = ({ servicio, setNext }) => {
   const dispatch = useDispatch();
 
-  const handleServiceChange = (event) => {
-    const servicio = event.target.value;
-    console.log(servicio);
-    // if (servicio === 0) return console.log("por favor ingrese una fecha");
+  useEffect(() => {
+    servicio === "" ? setNext(true) : setNext(false);
+  }, [servicio, setNext]);
 
+  const handleServiceChange = (event) => {
+    const nuevoServicio = event.target.value;
+    dispatch(onService(nuevoServicio));
     setNext(false);
-    dispatch(onService(servicio));
   };
   return (
     <StepperLayout>
       <Typography>Servicio: {servicio}</Typography>
-      <TextField type="number" onChange={handleServiceChange} />
+      <TextField
+        type="number"
+        onChange={handleServiceChange}
+        value={servicio}
+      />
     </StepperLayout>
   );
 };
