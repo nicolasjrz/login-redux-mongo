66a1aff8890581763b9e7941c40fc777dfc2f751
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-13 19:29:05 -0300

    nueva logica para isClosedDay

--- src/hooks/calendar/useDate.js
+++ src/hooks/calendar/useDate.js
@@ -7,6 +7,9 @@ export const useDate = (day, mes, anio, disabledDates = []) => {
 
   const isTodayDate = isSameDay(date, new Date());
   //const isWeekendDate = date.getDay() === 0; // Deshabilita si es domingo (0 representa domingo, 1 representa lunes, etc.)
+  //   const closedDays = [0, 1]; // Array que contiene los números de los días a deshabilitar (0 para domingo, 1 para lunes, etc.)
+  // const isClosedDay = closedDays.includes(date.getDay()); // Verifica si el número del día de la semana está en el array de días cerrados
+
   const isClosedDay = date.getDay() === 0 || date.getDay() === 1; // Deshabilita si es domingo o lunes
   const isPastDay = isBefore(date, new Date());
   const isDisabledDate = disabledDates.some((disabledDate) =>
